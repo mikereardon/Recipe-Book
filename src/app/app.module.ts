0cc046ba990177accaefafb1f66e604e6184ca13
Mike Reardon <michaelpjreardon@gmail.com>
2017-02-13 21:59:30 +0000

    Add router links to navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,8 @@ import { DropdownDirective } from './dropdown.directive';
 import { RecipeService } from "./recipes/recipe.service";
 import { ShoppingListService } from "./shopping-list/shopping-list.service";
 
+import { routing } from "./app.routing";
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -31,7 +33,8 @@ import { ShoppingListService } from "./shopping-list/shopping-list.service";
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routing
   ],
   providers: [RecipeService, ShoppingListService],
   bootstrap: [AppComponent]
