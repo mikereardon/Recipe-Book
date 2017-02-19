a9e0f0bd2bb6d95218ef2205524c0d20c2b44c4e
Mike Reardon <michaelpjreardon@gmail.com>
2017-02-19 17:24:46 +0000

    Implement recipe storage/ement recipe storage/retrieval via Firebase backend

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,9 +1,13 @@
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
 import { Recipe } from "./recipe";
 import { Ingredient } from "../ingredient";
+import { Headers, Http, Response } from "@angular/http";
+import 'rxjs/Rx';
 
 @Injectable()
 export class RecipeService {
+
+recipesChanged = new EventEmitter<Recipe[]>();
 
 private recipes: Recipe[] = [
   new Recipe('Schnitzel', 'Very Tasty', 'http://thumbs1.ebaystatic.com/d/l225/m/mfXELL6zPWJE4OC0agiXMZw.jpg',
@@ -11,7 +15,7 @@ private recipes: Recipe[] = [
     new Ingredient('Pork Meat', 1)]),
   new Recipe('Summer Salad', 'Okayish', 'http://thumbs1.ebaystatic.com/d/l225/m/mfXELL6zPWJE4OC0agiXMZw.jpg', [])
 ];
-  constructor() { }
+  constructor(private http: Http) { }
 
   getRecipes() {
     return this.recipes;
@@ -31,6 +35,25 @@ private recipes: Recipe[] = [
 
   editRecipe(oldRecipe: Recipe, newRecipe: Recipe) {
     this.recipes[this.recipes.indexOf(oldRecipe)] = newRecipe;
+  }
+
+  storeData() {
+    const body = JSON.stringify(this.recipes);
+    const headers = new Headers({
+      'Content-Type': 'application/json'
+    });
+    return this.http.put('https://recipebook-abde8.firebaseio.com/recipes.json', body, {headers: headers});
+  }
+
+  fetchData() {
+    return this.http.get('https://recipebook-abde8.firebaseio.com/recipes.json')
+      .map((response: Response) => response.json())
+      .subscribe(
+        (data: Recipe[]) => {
+          this.recipes = data;
+          this.recipesChanged.emit(this.recipes);
+        }
+      );
   }
 
 }
